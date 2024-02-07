16f4a58283087dd5bcab2e5bce7e1180510d0d27
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-02-07 14:30:19 +0600

    test

---
+++ index.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+
+const createTransporter = () => {
+  return nodemailer.createTransport({
+    service: process.env.NODEMAIL_SERVICE,
+    auth: {
+      user: process.env.NODEMAIL_EMAIL,
+      pass: process.env.NODEMAIL_PASSWORD,
+    },
+  });
+};
+
+const sendAnyEmail = async (email, subject, htmlContent) => {
+  try {
+    const transporterInstance = createTransporter(); // Use a different variable name here
+
+    const mailOptions = {
+      from: process.env.NODEMAIL_EMAIL,
+      to: email,
+      subject,
+      html: htmlContent,
+    };
+
+    await transporterInstance.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+module.exports = { sendAnyEmail };
